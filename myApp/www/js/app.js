720ba7cfd444cbb5b211b6d36706c50edd92127e
Simão Menezes <simaomenezesneto@gmail.com>
2016-10-29 11:11:50 -0200

    Criando pagina de detalhe referente ao item selecionado [  ]

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -50,15 +50,26 @@ angular.module('starter', ['ionic'])
         }
       }
     })
+
+    .state('tabs.detail', {
+      url: '/list/:aId',
+      views: {
+        'list-tab' :{
+          templateUrl: 'templates/detail.html',
+          controller: 'ListController'
+        }
+      }
+    })
     //rota default
     $urlRouterProvider.otherwise('/tab/home');
 
 })
 
-.controller('ListController', ['$scope', '$http', function($scope, $http) {
+.controller('ListController', ['$scope', '$http', '$state', function($scope, $http, $state) {
     $http.get('js/data.json').success(function(data) 
     {
       $scope.artists = data;
+      $scope.whichartist=$state.params.aId;
 
       $scope.toggleStar = function(item) {
         item.star = !item.star; 
